7b7fc0a2db6b20427d948b710f78460bcbe595ad
TizianoN <hello@tizianonicolai.com>
2026-02-27 12:37:09 +0100

    add first route

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express();
 const port = 3000;
 const appUrl = `http://localhost:${port}`;
 
+app.get("/", (req, res) => {
+  res.json({
+    message: "Benvenuto alla Pizzeria Express",
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server listenting on ${appUrl}`);
 });
